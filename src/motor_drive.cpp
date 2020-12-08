7001338add94c68c2adbfde356bcf1ca15f68fc9
sentry5588 <sentry5588@gmx.com>
2020-12-07 22:51:59 -0500

    Changed motor_drive node frequency to 50hz; work in progress on creating custom motor drive diagnostic ROS messages

--- src/motor_drive.cpp
+++ src/motor_drive.cpp
@@ -3,14 +3,15 @@
 #include <sensor_msgs/Imu.h>
 #include <cmath>
 
+
 double pitch_angle_comp_filtered = 0.0;
 
 void sub_read_imu(const sensor_msgs::Imu& msg)
 {
   double p_accel = 0.0, dp_gyro = 0.0, dT = 0.02;
   double alpha = 0.98; // To tune
-  ROS_INFO_STREAM("I heard: " << msg.header.frame_id);
-  ROS_INFO_STREAM("x acc: " << msg.linear_acceleration.x);
+//  ROS_INFO_STREAM("I heard: " << msg.header.frame_id);
+//  ROS_INFO_STREAM("x acc: " << msg.linear_acceleration.x);
   p_accel = atan2(msg.linear_acceleration.x, msg.linear_acceleration.z) - 0.205; // radian
   dp_gyro = msg.angular_velocity.y;
   pitch_angle_comp_filtered = (1-alpha) * p_accel + alpha * (pitch_angle_comp_filtered + dp_gyro * dT);
@@ -22,9 +23,13 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "motor_drive");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("imu", 1000, sub_read_imu);
-  ros::spin();
+  ros::Rate rate(50); // Hz
 
-  ROS_INFO_STREAM("*** motor_drive main function");
+  while(ros::ok()) {
+    ROS_INFO_STREAM("motor_drive main function");
+    ros::spinOnce();
+    rate.sleep();
+  }
 
   return 0;
 }
