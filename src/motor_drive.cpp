41efeafccbb6104fd4ff4e1b7369fa62a1c00ad1
sentry5588 <sentry5588@gmx.com>
2020-12-08 23:22:13 -0500

    Added motor drive diagnostic publisher; fixed mistake in using atan2 function to measure pitch angle

--- src/motor_drive.cpp
+++ src/motor_drive.cpp
@@ -2,9 +2,10 @@
 #include "std_msgs/String.h"
 #include <sensor_msgs/Imu.h>
 #include <cmath>
-
+#include "motor_drive/motor_drive_diagnostics.h"
 
 double pitch_angle_comp_filtered = 0.0;
+motor_drive::motor_drive_diagnostics m_diag_msg;
 
 void sub_read_imu(const sensor_msgs::Imu& msg)
 {
@@ -12,10 +13,16 @@ void sub_read_imu(const sensor_msgs::Imu& msg)
   double alpha = 0.98; // To tune
 //  ROS_INFO_STREAM("I heard: " << msg.header.frame_id);
 //  ROS_INFO_STREAM("x acc: " << msg.linear_acceleration.x);
-  p_accel = atan2(msg.linear_acceleration.x, msg.linear_acceleration.z) - 0.205; // radian
+  p_accel = atan2(msg.linear_acceleration.z, msg.linear_acceleration.x) - 0.205; // radian
   dp_gyro = msg.angular_velocity.y;
   pitch_angle_comp_filtered = (1-alpha) * p_accel + alpha * (pitch_angle_comp_filtered + dp_gyro * dT);
-  ROS_INFO_STREAM("Filtered pitch angle is: " << pitch_angle_comp_filtered); 
+  ROS_INFO_STREAM("Filtered pitch angle is: " << pitch_angle_comp_filtered);
+
+  m_diag_msg.pitch_ang_meas = p_accel;
+  m_diag_msg.pitch_ang_rate = dp_gyro;
+  m_diag_msg.x_accel = msg.linear_acceleration.x;
+  m_diag_msg.z_accel = msg.linear_acceleration.z;
+  m_diag_msg.pitch_ang_filtered = pitch_angle_comp_filtered;
 }
 
 int main(int argc, char **argv)
@@ -23,9 +30,14 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "motor_drive");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("imu", 1000, sub_read_imu);
+  ros::Publisher pub = n.advertise<motor_drive::motor_drive_diagnostics>("compl_filter_diag", 10);
   ros::Rate rate(50); // Hz
 
+  
+
   while(ros::ok()) {
+    pub.publish(m_diag_msg);
+
     ROS_INFO_STREAM("motor_drive main function");
     ros::spinOnce();
     rate.sleep();
