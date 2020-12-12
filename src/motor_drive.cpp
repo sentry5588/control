60b945d8c5898cd116d74db0dd460ae02074af36
sentry5588 <sentry5588@gmx.com>
2020-12-12 17:55:26 -0500

    Failed attempt to use wiringPi lib for motor drive

--- src/motor_drive.cpp
+++ src/motor_drive.cpp
@@ -3,9 +3,11 @@
 #include <sensor_msgs/Imu.h>
 #include <cmath>
 #include "motor_drive/motor_drive_diagnostics.h"
+#include <wiringPi.h>
 
 double pitch_angle_comp_filtered = 0.0;
 motor_drive::motor_drive_diagnostics m_diag_msg;
+int PWM_RANGE = 4095, PWM_DUTY_CYCLE = 2047;
 
 void sub_read_imu(const sensor_msgs::Imu& msg)
 {
@@ -25,6 +27,23 @@ void sub_read_imu(const sensor_msgs::Imu& msg)
   m_diag_msg.pitch_ang_filtered = pitch_angle_comp_filtered;
 }
 
+void set_left_wheel_speed(double v) {
+  int PWM_PIN = 18, DIRECTION_PIN = 24;
+  int clock_divider = 1000;
+//  pinMode(PWM_PIN, PWM_OUTPUT);
+//  pinMode(DIRECTION_PIN, OUTPUT);
+
+  if (v >= 0.0) {
+//    digitalWrite(DIRECTION_PIN, HIGH);
+  } else {
+//    digitalWrite(DIRECTION_PIN, LOW);
+  }
+//  pwmSetMode(PWM_MODE_MS);
+//  pwmSetRange (PWM_RANGE);
+//  pwmWrite(PWM_PIN, PWM_DUTY_CYCLE);
+  pwmSetClock(clock_divider);
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "motor_drive");
@@ -33,10 +52,14 @@ int main(int argc, char **argv)
   ros::Publisher pub = n.advertise<motor_drive::motor_drive_diagnostics>("compl_filter_diag", 10);
   ros::Rate rate(50); // Hz
 
-  
+  if (wiringPiSetupGpio() == -1) {
+    exit(1);
+  }
 
   while(ros::ok()) {
+    set_left_wheel_speed(m_diag_msg.pitch_ang_filtered);
     pub.publish(m_diag_msg);
+
 
     ROS_INFO_STREAM("motor_drive main function");
     ros::spinOnce();
